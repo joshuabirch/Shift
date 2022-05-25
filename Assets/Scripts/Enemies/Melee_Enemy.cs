b51ef9a33da917f5f44e1b626be8a7b586888b33
G1620242 <87494709+Benjidgs@users.noreply.github.com>
2022-05-25 19:42:15 +0100

    Cleanup&firstEnemy

--- Assets/Scripts/Enemies/Melee_Enemy.cs
+++ Assets/Scripts/Enemies/Melee_Enemy.cs
@@ -1,9 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Melee_Enemy : MonoBehaviour
 {
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    //[SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,32 @@ public class Melee_Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        cooldownTimer += Time.deltaTime;
+        if (PlayerVisual())
+        {   
+            if (cooldownTimer >= attackCooldown)
+            {
+            //anim (animator) goes here
+            cooldownTimer = 0;
+             Debug.Log("Attack");
+            }
+        }
     }
+
+    private bool PlayerVisual()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z) 
+        , 0,Vector2.left ,0 ,playerLayer);
+        return hit.collider != null;
+    }
+
+private void OnDrawGizmos()
+{
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+}
+
 }
