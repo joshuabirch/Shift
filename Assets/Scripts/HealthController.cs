cc305f45f7d98e01825c0a38062bd9325e2fd1bb
G1620242 <87494709+Benjidgs@users.noreply.github.com>
2022-05-26 18:17:23 +0100

    Enemy-SimpleMovement-and-AttackfixAttempt

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,28 +1,30 @@
 using UnityEngine;
+using System.Collections;
 
 public class HealthController : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     [SerializeField] public float currentHealth; //{get; private set;}
+    //[SerializeField] private int damage;
 
     private void Awake()
     {
         currentHealth = startingHealth;
     }
 
-    public void TakeDamage(float damage)
+    public void TakeDamage(float _damage)
     {
         //currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
 
-        if (currentHealth > 0)
-        {
-            currentHealth -= damage;
-        }
+       // if (currentHealth > 0)
+       // {
+            currentHealth -= _damage;
+        //}
 
-        else
-        {
-            currentHealth -= damage;
-        }
+        //else
+       // {
+           // currentHealth -= damage;
+       // }
     }
 
     // Start is called before the first frame update
@@ -34,6 +36,9 @@ public class HealthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if(Input.GetKeyDown(KeyCode.E))
+        // {
+        //     TakeDamage(damage);
+        // }
     }
 }
